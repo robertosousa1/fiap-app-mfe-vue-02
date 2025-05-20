6584c30739de01197aa147132993b812b9a89e1e
Roberto Alves <roberto_junior_sousa@hotmail.com>
2025-05-20 09:30:16 -0300

    add mfe

--- src/mfe-entry.js
+++ src/mfe-entry.js
@@ -5,7 +5,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 export function mount(container) {
-  console.log();
   const app = createApp(App);
   app.mount(container);
 }
