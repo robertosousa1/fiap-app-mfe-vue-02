1a9e5832085104070a7e2e193a103c057ac14db1
Roberto Alves <roberto_junior_sousa@hotmail.com>
2025-05-20 08:33:17 -0300

    add mfe

--- src/mfe-entry.js
+++ src/mfe-entry.js
@@ -1,4 +1,8 @@
+import { createApp } from "vue";
 import App from "./App.vue";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 export function mount(container) {
   const app = createApp(App);
